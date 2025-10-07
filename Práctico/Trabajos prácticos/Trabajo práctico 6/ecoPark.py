9ac9a52a69631541c8fd048f6f526f05fc7b3bfd
Tomas Olmos <95538@sistemas.frc.utn.edu.ar>
2025-10-07 13:43:06 -0300

    refactor - test: acepta terminos

--- Práctico/Trabajos prácticos/Trabajo práctico 6/ecoPark.py
+++ Práctico/Trabajos prácticos/Trabajo práctico 6/ecoPark.py
@@ -2,6 +2,11 @@
 def inscribirse(actividad, horario, participantes):
     # Verificar que todos los participantes acepten los términos y condiciones
     for participante in participantes:
-        if not participante.get("acepta_terminos", False):
+        if not acepta_terminos(participante):
             return {"ok": False, "mensaje": "Debe aceptar los términos y condiciones"}
     return {"ok": True, "mensaje": "Inscripción exitosa"}
+
+def acepta_terminos(participante):
+    if participante.get("acepta_terminos", False):
+        return True
+    return False
