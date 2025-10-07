839d42a7422f17618f1856b68cd1a6346d60d9b5
Tomas Olmos <95538@sistemas.frc.utn.edu.ar>
2025-10-07 13:37:20 -0300

    RED - Test: Acepta Terminos

---
+++ Práctico/Trabajos prácticos/Trabajo práctico 6/tests.py
@@ -0,0 +1,11 @@
+import pytest
+
+def test_acepta_terminos_y_condiciones():
+    resultado = inscribirse(
+        actividad = "Tirolesa", 
+        horario = "10:00",
+        participantes = [{"nombre": "Toto", "edad": 10, "acepta_terminos": True}]
+    )
+    assert resultado["ok"] is True
+    assert resultado["mensaje"] == "Inscripción exitosa"
+
