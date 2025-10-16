4e115cb7b497aa2acd9b1dbeebb5da2c17178cdb
Julieta Brussa <jbrussa01@gmail.com>
2025-10-16 17:02:37 -0300

    Borrar comentarios

--- Práctico/Trabajos prácticos/Trabajo práctico 6/tests.py
+++ Práctico/Trabajos prácticos/Trabajo práctico 6/tests.py
@@ -2,30 +2,12 @@ import pytest
 from ecoPark import inscribirse
 from actividad import Actividad
 
-# def test_acepta_terminos_y_condiciones():
-#    resultado = inscribirse(
-#        actividad = "Tirolesa", 
-#        horario = "10:00",
-#        participantes = [{"nombre": "Toto", "edad": 10, "acepta_terminos": True}]
-#    )
-#    assert resultado["ok"] is True
-#    assert resultado["mensaje"] == "Inscripción exitosa"
-
 def test_no_acepta_terminos_y_condiciones():
     actividad = Actividad("Tirolesa")
     actividad.agregar_disponibilidad("2025-10-10", {"10:00": 5, "11:00": 0})
     resultado = inscribirse(actividad, "2025-10-10", "11:00", [{"nombre": "Toto", "acepta_terminos": False}])
     assert resultado["ok"] is False
     assert resultado["mensaje"] == "Debe aceptar los términos y condiciones"
-
-# def test_inscripcion_con_cupos_disponibles():
-#    resultado = inscribirse(
-#        actividad="Tirolesa",
-#        horario="10:00",
-#        participantes=[{"nombre": "Toto", "edad": 10, "acepta_terminos": True}]
-#    )
-#    assert resultado["ok"] is True
-#    assert resultado["mensaje"] == "Inscripción exitosa"
 
 def test_sin_cupos_para_fecha_horario():
     actividad = Actividad("Tirolesa")
